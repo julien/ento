50791fddb363c00a5fa9ccfc4968c067c7b2585c
Julien Castelain <julien@users.noreply.github.com>
2023-10-31 21:37:39 +0100

    Add from_file function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use std::convert::TryFrom;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 #[derive(Debug)]
 pub struct Variable {
     pub key: String,
@@ -17,5 +22,32 @@ impl TryFrom<String> for Variable {
                 val: results[1].to_string(),
             })
         }
+    }
+}
+
+pub fn from_file(path: String) -> Result<Vec<Variable>, &'static str> {
+    let file = File::open(path);
+    if file.is_err() {
+        return Err("Couldn't open file");
+    }
+
+    let reader = BufReader::new(file.unwrap());
+    let mut variables: Vec<Variable> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap_or_else(|_| "".to_string());
+        let variable: Result<Variable, &str> = Variable::try_from(line);
+        if variable.is_err() {
+            continue;
+        }
+        let variable = variable.unwrap();
+        env::set_var(&variable.key, &variable.val);
+        variables.push(variable);
+    }
+
+    if variables.is_empty() {
+        Err("Couldn't find any environment variables")
+    } else {
+        Ok(variables)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,18 @@
-use std::{
-    convert::TryFrom,
-    env,
-    fs::File,
-    io::{self, BufRead, BufReader},
-    process,
-};
+use std::{env, io, process};
 
 fn main() -> io::Result<()> {
-    let filepath = env::args().nth(1);
-    if filepath.is_none() {
-        eprintln!("usage ento <filename>");
+    let path = env::args().nth(1);
+    if path.is_none() {
+        eprintln!("usage: ento <filename>");
         process::exit(1);
     }
 
-    let file = File::open(filepath.unwrap())?;
-    let reader = BufReader::new(file);
-    let mut variables: Vec<ento::Variable> = Vec::new();
-
-    for line in reader.lines() {
-        let line = line?;
-        let variable: Result<ento::Variable, &str> = ento::Variable::try_from(line);
-        if variable.is_err() {
-            continue;
-        }
-        let variable = variable.unwrap();
-        env::set_var(&variable.key, &variable.val);
-        variables.push(variable);
-    }
-
-    if variables.is_empty() {
-        eprintln!("couldn't find any environment variables");
+    let variables: Result<Vec<ento::Variable>, &str> = ento::from_file(path.unwrap());
+    if variables.is_err() {
+        eprintln!("error: {:?}", variables.unwrap());
         process::exit(1);
     }
+    let variables = variables.unwrap();
 
     println!("found and set the following environment variables");
     for variable in variables.iter() {
