5abcdb5d9fb8c5cc1e2fbaef7dc9029ebc9a03f1
Julien Castelain <julien@users.noreply.github.com>
2023-11-03 20:14:05 +0100

    Change output messages
    This is just an example, not a binary

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ use std::{env, io, process};
 fn main() -> io::Result<()> {
     let path = env::args().nth(1);
     if path.is_none() {
-        eprintln!("usage: ento <filename>");
+        eprintln!("Pass a file name as the first argument");
         process::exit(1);
     }
 
     let variables: Result<Vec<Variable>, &str> = from_file(path.unwrap());
     if variables.is_err() {
-        eprintln!("error: {:?}", variables.unwrap());
+        eprintln!("Error: {:?}", variables.unwrap());
         process::exit(1);
     }
     let variables = variables.unwrap();
 
-    println!("found and set the following environment variables");
+    println!("Found and set the following environment variables");
     for variable in variables.iter() {
         let s: String = variable.into();
         println!("{}", s);
